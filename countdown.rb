eefdbc0aae61dd1d51e0d9a07b3099d5eff377b3
victoriaehrbar <victoriaehrbar@gmail.com>
2020-11-10 03:27:59 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -7,3 +7,12 @@ while seconds_to_midnight > 0
 end
 "HAPPY NEW YEAR!"
 end
+
+def countdown_with_sleep(seconds_to_midnight)
+  while seconds_to_midnight > 0
+    puts "#{seconds_to_midnight} SECOND(S)!"
+    sleep(1)
+    seconds_to_midnight -= 1
+  end
+  "HAPPY NEW YEAR!"
+end
